0231dbd1978c73570b629b726ed36dbb5ffd7b1a
ashish kumar <ak3616272@gmail.com>
2023-07-29 07:20:49 +0530

    initial commit

---
+++ test_app/src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Style from "../styles/index.module.css"
+
+const index = () => {
+  return (
+    <div className={Style.main}>
+        <h1 className={Style.h1}>Index page</h1>
+    </div>
+  )
+}
+
+export default index
